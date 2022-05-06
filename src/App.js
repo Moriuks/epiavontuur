e62a3232554e524a57a1232d2505821c6bf0739d
Luis Mora <luismora371@gmail.com>
2022-05-06 08:58:13 -0500

    added initial navBar component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
 import logo from './logo.svg';
+import { BrowserRouter, Routes, Route, Navigate, Link } from 'react-router-dom';
 import './App.scss';
 import Greet from './components/Greet';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <Greet/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar/>
     </div>
   );
 }
---
+++ src/components/NavBar/index.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container'
+import NavDropdown from 'react-bootstrap/NavDropdown'
+import Nav from 'react-bootstrap/Nav'
+import './NavBar.scss';
+
+function NavBar(){
+
+    return(
+        <Navbar  
+          bg="light"
+          container="fluid"
+          expand="md"
+          fixed="top"
+          className="Epi-Navbar"
+        >
+        <Container>
+            <Navbar.Brand href="#home" id="NavBar-Title">EPI AVONTUUR</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto Epi-departments">
+                <NavDropdown title="Nosotros" id="basic-nav-dropdown">
+                    <NavDropdown.Item href="#action/1.1">Equipo</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/1.2">Nosotros</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/1.3">Carreras</NavDropdown.Item>
+                </NavDropdown>
+                    <NavDropdown title="Soluciones" id="basic-nav-dropdown">
+                    <NavDropdown.Item href="#action/3.1">Infraestructura</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.2">Aviación</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.3">Solución Satelital</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.3">Equipamiento y Tecnología</NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#action/3.4">Otros</NavDropdown.Item>
+                </NavDropdown>
+                <Nav.Link href="#link">Clientes</Nav.Link>
+                <Nav.Link href="#link">FAQ's</Nav.Link>
+            </Nav>
+            </Navbar.Collapse>
+        </Container>
+        </Navbar>
+    )
+}
+
+export default NavBar
