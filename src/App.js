1a06aad6d61345480922b0f82c7d167979f24af5
Luis Mora <luismora371@gmail.com>
2022-05-13 18:59:11 -0500

    update

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           <Route path='*'>
             <NotFound />
           </Route>
-          <Route path='/working'>
+          <Route path='/'>
             <Working />
           </Route> 
           <Route path='/working'>
