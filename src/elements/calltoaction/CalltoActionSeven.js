1588d3a139f00a286c969f1454423a54599da0f5
Luis Mora <luismora371@gmail.com>
2022-05-20 14:47:45 -0500

    call to action update

--- src/elements/calltoaction/CalltoActionSeven.js
+++ src/elements/calltoaction/CalltoActionSeven.js
@@ -4,9 +4,9 @@ import { FiArrowRight } from "react-icons/fi";
 import Logo from "../logo/Logo";
 
 const callToActionData = {
-    title: "Ready to start creating a standard website?",
-    subtitle: "If you want to create a corporate template you can purshace now our SoftwareChido template.",
-    btnText: "Purchase SoftwareChido",
+    title: "¿Listo para la transformación digital?",
+    subtitle: "Aun no estas seguro, pero te intriga conocer más...",
+    btnText: "Contacto Epiavn",
 }
 
 const CalltoActionSeven = () => {
