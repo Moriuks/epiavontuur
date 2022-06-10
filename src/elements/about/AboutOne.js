989a92528e1dd1c65340952002a7f022ca00ff1d
Luis Mora <luismora371@gmail.com>
2022-06-10 04:38:14 -0500

    updated about one. no 5

--- src/elements/about/AboutOne.js
+++ src/elements/about/AboutOne.js
@@ -18,7 +18,7 @@ const AboutOne = () => {
                                 <h2 className="title">EPI Avontuur <br /> {" "}
                                     <Typed className="theme-gradient"
                                         strings={[
-                                            "Consultaría.",
+                                            "Consultoría.",
                                             "Infraestuctura.",
                                             "Agencia.",
                                         ]}
@@ -28,7 +28,7 @@ const AboutOne = () => {
                                         loop
                                     />
                                 </h2>
-                                <p>El equipo técnico y de operaciones interno de EPI está formado por expertos de la industria, cuatro de los cuales tienen doctorados en nuestras áreas comerciales principales. Además, EPI cuenta con más de 2500 consultores asociados que provienen de 70 países diferentes para garantizar la combinación correcta de experiencia.</p>
+                                <p>Contamos con un equipo de especialistas conformado por expertos con más de 35 años de experiencia en  realizando Obra Civil, Instalaciones Electromecánicas, Instalaciones Hidráulicas.</p>
                                 <p>EPI utiliza las últimas tecnologías y técnicas para diseñar soluciones a medida para los proyectos más desafiantes. Es importante destacar que poseemos la capacidad de ofrecer un conjunto integrado de servicios para todo su programa de trabajo, con capacidades que cubren el ciclo de vida completo del proyecto. Tenemos un historial excepcional de calidad, innovación y éxito, y estaremos con usted en cada paso del camino. </p>
                                 <div className="read-more-btn mt--40">
                                     <a className="btn-default" href="#"><span>Más sobre nosotros</span></a>
